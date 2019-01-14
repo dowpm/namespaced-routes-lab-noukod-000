562dec45d9c480d2cb178bf592deeaf50c570b8a
Mac Dowell Prosper <macdowell@noukod.com>
2019-01-14 18:50:38 +0000

    Automatically backed up by Learn

--- db/migrate/20190114173937_create_preferences.rb
+++ db/migrate/20190114173937_create_preferences.rb
@@ -1,6 +1,8 @@
 class CreatePreferences < ActiveRecord::Migration
   def change
     create_table :preferences do |t|
+      t.string :artist_sort_order
+      t.string :song_sort_order
       t.boolean :allow_create_artists
       t.boolean :allow_create_songs
     end

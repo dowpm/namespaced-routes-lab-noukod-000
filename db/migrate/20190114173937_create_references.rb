5e9393b342234b9625b882f239b82f7179e9d3e7
Mac Dowell Prosper <macdowell@noukod.com>
2019-01-14 17:40:25 +0000

    Automatically backed up by Learn

--- db/migrate/20190114173937_create_references.rb
+++ db/migrate/20190114173937_create_references.rb
@@ -1,6 +1,8 @@
 class CreateReferences < ActiveRecord::Migration
   def change
     create_table :references do |t|
+      t.boolean :allow_create_artist
+      t.boolean :allow_create_song
     end
   end
 end
